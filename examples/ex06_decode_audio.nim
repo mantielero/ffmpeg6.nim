595a16ddbdfde007135d170812a931f6edd9d93e
mantielero <josemaria.alkala@gmail.com>
2024-04-29 21:11:31 +0200

    not working well

--- examples/ex06_decode_audio.nim
+++ examples/ex06_decode_audio.nim
@@ -1,6 +1,16 @@
 # https://github.com/FFmpeg/FFmpeg/blob/master/doc/examples/decode_audio.c
 # Decode data from an MP2 input file and generate a raw audio file to
 # be played with ffplay.
+#[
+$ cc  -Wall -g   -c -o encode_audio.o encode_audio.c
+$ cc   decode_audio.o  -lavdevice -lavformat -lavfilter -lavcodec -lswresample -lswscale -lavutil  -o decode_audio
+
+$ ./decode_audio media/sample3.mp2 media/audio.raw
+Warning: the sample format the decoder produced is planar (s16p). This example will output the first channel only.
+Play the output audio file with the command:
+ffplay -f s16le -ac 2 -ar 44100 media/audio.raw
+
+]#
 import ffmpeg6
 import std/[streams, posix]
 
@@ -9,6 +19,13 @@ const
   AUDIO_REFILL_THRESH = 4096
 const 
   AV_NOPTS_VALUE:int64 = 0x80000000000# -92233720368547(valor mínimo que puede almacenarse)
+
+ 
+#define AVERROR_EOF   FFERRTAG( 'E','O','F',' ')
+#define 	FFERRTAG(a, b, c, d)   (-(int)MKTAG(a, b, c, d))
+#define MKTAG	(a,b,c,d)  ((a) | ((b) << 8) | ((c) << 16) | ((unsigned)(d) << 24))
+const
+  AVERROR_EOF = -541478725
 
 
 type
@@ -110,12 +127,11 @@ proc decode(decCtx: CodecContextRef;
   while ret >= 0:
     # https://www.ffmpeg.org/doxygen/trunk/group__lavc__decoding.html#ga11e6542c4e66d3028668788a1a74217c
     ret = avcodec_receive_frame(decCtx.handle, decodedFrame) 
-    echo " ---> ", ret
-    if ret == -EAGAIN: # 	output is not available in this state - user must try to send new input
+    #echo " ---> ", ret
+    if ret == -EAGAIN or ret == AVERROR_EOF: # 	output is not available in this state - user must try to send new input
       #raise newException(ValueError, "Error during decoding")
       break
-    # if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-    #     return;
+
    
     elif ret < 0:  # legitimate decoding errors
       raise newException(ValueError, "Error during decoding")
@@ -130,17 +146,29 @@ proc decode(decCtx: CodecContextRef;
       # This should not occur, checking just for paranoia
       raise newException(ValueError, "Failed to calculate data size")
     
-    #echo "Number of samples : ", decodedFrame.nb_samples
+    echo "  Number of decompressed bytes : ", decodedFrame.nb_samples * decCtx.handle.ch_layout.nb_channels
+    #echo AVERROR_EOF
     for i in 0..<decodedFrame.nb_samples:
       #echo "Number of channels : ", decCtx.handle.ch_layout.nb_channels
       for ch in 0..<decCtx.handle.ch_layout.nb_channels:
         #var channelOrigin:ptr uint8 = decodedFrame.data[ch]
         #var pos = cast[pointer]( cast[int](channelOrigin) +  data_size * i ) # 
-        f.writeData(cast[pointer](cast[int](decodedFrame.data[ch]) + data_size*i), data_size)
+        # f.writeData(cast[pointer](cast[int](decodedFrame.data[ch]) + data_size*i), 
+        #             data_size )
+        # var tmp1 = cast[uint8](cast[pointer](cast[int](decodedFrame.data[ch]) + data_size*i))
+        # var tmp2 = cast[uint8](cast[int](decodedFrame.data[ch]) + data_size*i)
+        #var tmp3 = cast[ptr uint8](cast[int](decodedFrame.data[ch]) + data_size*i)
+        #echo tmp1, " ", tmp2, " ", tmp3[]
+        f.writeData(cast[ptr uint8](cast[int](decodedFrame.data[ch]) + data_size*i), data_size )
+        echo cast[ptr uint8](cast[int](decodedFrame.data[ch]) + data_size*i)[]
+        #f.write(tmp3[])
         #f.writeData(pos, data_size)
         #var val = cast[uint8](pos)
         #f.write(val)
+
         # fwrite(frame->data[ch] + data_size*i, 1, data_size, outfile);
+
+        
     #var ch1 = cast[ptr UncheckedArray[ptr uint8]](decodedFrame.data[ch])
 
 
@@ -189,20 +217,20 @@ proc main =
                                 AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0)
     if ret < 0:
       raise newException(ValueError, "error while parsing")                                  
-    echo "  leidos: ", ret, " bytes"
+    echo "  the number of bytes of the input bitstream used: ", ret, " bytes"
     ini += ret
-    echo "  Tamaño comprimido: ", pkt.size
+    echo "  Tamaño comprimido: ", pkt.size, " bytes"
     if pkt.size > 0:
       decode(c, pkt, outfile)
     echo "  new Ini/data_size", (ini,data_size)
     remainingData = data_size - ini
-    echo "remainingData vs AUDIO_REFILL: ", (remainingData, AUDIO_REFILL_THRESH)
+    #echo "  remainingData vs AUDIO_REFILL: ", (remainingData, AUDIO_REFILL_THRESH)
 
     #if remainingData < 0:
     #  break
 
     if remainingData < AUDIO_REFILL_THRESH and remainingData > 0: # threshold used to read more data
-      echo "Need to refill!"
+      echo "  Need to refill!"
       # Copy the remaning data to the beginning
       for i in 0..<remainingData:
         inbuf[i] = inbuf[ini + i]
@@ -211,39 +239,31 @@ proc main =
       # cast[pointer](cast[int](inbuf[0]) + remainingData)
       var len = f.readData(inbuf[remainingData].addr, # refill without overwritting
                             AUDIO_INBUF_SIZE - remainingData)
-      echo "   read: ", len
+      echo "     read from file: ", len, " bytes"
       if len >= 0:
         data_size = remainingData + len
         remainingData = data_size
         nbytes += len
+      else:
+        raise newException(ValueError, "why negative len?")
 
-  echo "TOTAL bytes: ", nbytes
+  echo "TOTAL bytes: ", nbytes  # I looks like the file is properly read
   # flush the decoder
   pkt.data = nil
   pkt.size = 0
   #decode(c, pkt, outfile)
 
   # INFO
-  echo c.handle.sample_fmt
+  # print output pcm infomations, because there have no metadata of pcm
+  var samplingFormat = c.handle.sample_fmt
+  
+  if av_sample_fmt_is_planar(samplingFormat) == 0:
+    echo "Sampling format interleaved"
+  elif av_sample_fmt_is_planar(samplingFormat) == 1:
+    echo "Sampling format planar"
 
-
-
-main() 
 #[
-#include <libavutil/frame.h>
-#include <libavutil/mem.h>
- 
-#include <libavcodec/avcodec.h>
- 
 
- 
-
- 
-    # ---------------------------------------------
-
- 
-    /* print output pcm infomations, because there have no metadata of pcm */
-    sfmt = c->sample_fmt;
  
     if (av_sample_fmt_is_planar(sfmt)) {
         const char *packed = av_get_sample_fmt_name(sfmt);
@@ -261,6 +281,13 @@ main()
            "ffplay -f %s -ac %d -ar %d %s\n",
            fmt, n_channels, c->sample_rate,
            outfilename);
+]#
+main() 
+
+
+#[
+
+
 end:
     fclose(outfile);
     fclose(f);
