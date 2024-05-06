6663913a233abf88b3d26109054c67bbaa7496e6
mantielero <josemaria.alkala@gmail.com>
2024-05-06 22:37:59 +0200

    using a file from "media/"

--- examples/ex02_show_metadata.nim
+++ examples/ex02_show_metadata.nim
@@ -4,13 +4,12 @@ import ffmpeg6
 import std/os
 
 proc main =
-  var filename = "/home/jose/Vídeos/Chimera-AV1-10bit-1920x1080-6191kbps.mp4"
+  var filename = "media/sample-5s.mp4"
   if paramCount() != 1:
     echo """usage: %s <input_file>
       example program to demonstrate the use of the libavformat metadata API."""
   else:
     filename = paramStr(1)   
-  #echo paramCount(), " ", paramStr(1)
 
   var fmtCtx = getFormatContext(filename)
   fmtCtx.findStreamInfo()
