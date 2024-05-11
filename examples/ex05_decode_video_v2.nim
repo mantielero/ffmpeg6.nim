d4ec7a3a3999bcda5967032d4a76662f4e4a2b9e
mantielero <josemaria.alkala@gmail.com>
2024-05-11 18:35:27 +0200

    MPEG1 still hardcoded

--- examples/ex05_decode_video_v2.nim
+++ examples/ex05_decode_video_v2.nim
@@ -23,7 +23,7 @@ If not specified, the example is run creating:
     filename = paramStr(1)
     outFilename = paramStr(2)
 
-  # find the MPEG-1 video decoder
+  # find the MPEG-1 video decoder (hardcoded, so this won't work with a .mp4 file)
   var codec = findDecoder(AV_CODEC_ID_MPEG1VIDEO)
 
   # initialize the parse for codec MPEG-1
@@ -33,21 +33,25 @@ If not specified, the example is run creating:
   # For some codecs, such as msmpeg4 and mpeg4, width and height
   # MUST be initialized there because this information is not
   # available in the bitstream.
-
+  #var ret = avcodec_parameters_to_context(c.handle, stream.handle.codecpar)
+  if ret < 0:
+    raise newException(ValueError, "failed copying parameters from stream to codec context")
   # open it
   open(c, codec)
 
   var f = newFileStream(filename, fmRead)
 
   for pkt in f.getPackets(parser, c):
+    #if pkt.handle.streamindex == 
     var frame = pkt.decode(c)
-    if frame.handle.width > 0:
-      (&"{outFilename}_Y.pgm").savePgm(frame, 0)
-      (&"{outFilename}_U.pgm").savePgm2(frame, 1)
-      (&"{outFilename}_V.pgm").savePgm2(frame, 2)
-      (&"{outFilename}.ppm").savePpm(frame)
-    else:
-      echo "empty frame"
+    if frame != nil:
+      if frame.handle.width > 0:
+        (&"{outFilename}_Y.pgm").savePgm(frame, 0)
+        (&"{outFilename}_U.pgm").savePgm2(frame, 1)
+        (&"{outFilename}_V.pgm").savePgm2(frame, 2)
+        (&"{outFilename}.ppm").savePpm(frame)
+      else:
+        echo "empty frame"
 
 main()
 
